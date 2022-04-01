c822937f71d587c39d09ba5712877a8d80aabc3e
Nart Aliti <76398544+alitinart@users.noreply.github.com>
2022-04-01 21:05:52 +0200

    Header

--- src/pageComponents/Header/Header.tsx
+++ src/pageComponents/Header/Header.tsx
@@ -1,5 +1,31 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import Logo from "../../assets/images/BetterInstagram.png";
+
+import "../pageComponents.css";
 
 export default function Header() {
-  return <div className="Header"></div>;
+  return (
+    <div className="container computer-view header">
+      <img src={Logo} alt="BetterInstagram Logo" />
+      <input type={"text"} placeholder="Search" />
+      <div className="icons">
+        <NavLink to={"/"}>
+          <i className="bi bi-house icon"></i>
+        </NavLink>
+        <NavLink to={"/inbox"}>
+          <i className="bi bi-chat icon"></i>
+        </NavLink>
+        <NavLink to={"/add-post"}>
+          <i className="bi bi-plus-circle icon"></i>
+        </NavLink>
+        <NavLink to={"/discover"}>
+          <i className="bi bi-compass icon"></i>
+        </NavLink>
+        <NavLink to={"/profile"}>
+          <i className="bi bi-person icon"></i>
+        </NavLink>
+      </div>
+    </div>
+  );
 }
