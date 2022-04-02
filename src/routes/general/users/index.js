d1f755ea14b537bf55f30acb5b5765b6d6e93781
Nart Aliti <76398544+alitinart@users.noreply.github.com>
2022-04-02 16:04:45 +0200

    Sync token request

--- src/routes/general/users/index.js
+++ src/routes/general/users/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const authenticateToken = require("../../../middleware/authenticateToken");
 const checkAPIKey = require("../../../middleware/checkAPIKey");
+const tokenProvider = require("../../../services/tokenProvider");
 const User = mongoose.model("User");
 
 require("dotenv").config();
@@ -30,11 +31,12 @@ router.get("/", checkAPIKey, async (req, res) => {
  *
  */
 
-router.get("/", checkAPIKey, authenticateToken, (req, res) => {
+router.get("/sync", checkAPIKey, authenticateToken, (req, res) => {
   User.findOne({ _id: req.user._id }).then((user) => {
+    const newToken = tokenProvider(user);
     res.json({
       error: false,
-      data: { user },
+      data: { user, token: newToken },
       message: "User Synced",
     });
   });
